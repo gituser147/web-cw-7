43caac1228ab7b038fced507389e9b54a6b1d3db
adam alsaid kamal <adamzarzora9@gmail.com>
2022-03-23 20:27:15 +0300

    .

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,16 @@
 // project أنشئ كائن
-
+const project = {
+  type: "medical",
+  users: 10,
+  biography() {
+    return "it's a cbc (complete blood count)";
+  },
+};
 // randomProperty أنشئ دالة
+function randomProperty(obj) {
+  const keys = Object.keys(obj);
+  const random = Math.floor(Math.random() * keys.length);
+  console.log(keys[random]);
+  console.log(obj[`${keys[random]}`]);
+}
+randomProperty(project);
