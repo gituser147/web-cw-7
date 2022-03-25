1e38b95e1e9b32b7d7a1f9287b17035ff0e5ee79
adam alsaid kamal <adamzarzora9@gmail.com>
2022-03-25 22:25:44 +0300

    Update script.js

--- Random Password/script.js
+++ Random Password/script.js
@@ -1,28 +1,72 @@
 // Database قاعدة البيانات
 const chars = [
-  { 1: "0123456789" },
-  { 2: "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
-  { 3: "abcdefghijklmnopqrstuvwxyz" },
-  { 4: "+-*/!@#$%^&*()~|(){}[]<>;:'`.," },
+  { nums: "0123456789" },
+  { capLetters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+  { smallLetters: "abcdefghijklmnopqrstuvwxyz" },
+  { symbols: "+-*/!@#$%^&*()~|(){}[]<>;:'`.," },
 ];
 // دالة generate
 function generate() {
   let randompassword = "";
-  let collection_one = chars[0]["1"];
-  let collection_two = chars[1]["2"];
-  let collection_three = chars[2]["3"];
-  let collection_four = chars[3]["4"];
-  randompassword = randompassword = collection_one.charAt(
-    Math.floor(Math.random * collection_one.charAt.length)
-  );
-  randompassword = randompassword = collection_two.charAt(
-    Math.floor(Math.random * collection_two.charAt.length)
-  );
-  randompassword = randompassword = collection_three.charAt(
-    Math.floor(Math.random * collection_three.charAt.length)
-  );
-  randompassword = randompassword = collection_four.charAt(
-    Math.floor(Math.random * collection_four.charAt.length)
-  );
+  let collection_one = chars[0]["nums"];
+  let collection_two = chars[1]["capLetters"];
+  let collection_three = chars[2]["smallLetters"];
+  let collection_four = chars[3]["symbols"];
+  randompassword =
+    randompassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+  randompassword =
+    randompassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_two.length));
+  randompassword =
+    randompassword +
+    collection_three.charAt(
+      Math.floor(Math.random() * collection_three.length)
+    );
+  randompassword =
+    randompassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_four.length));
+  randompassword =
+    randompassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+  randompassword =
+    randompassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_two.length));
+  randompassword =
+    randompassword +
+    collection_three.charAt(
+      Math.floor(Math.random() * collection_three.length)
+    );
+  randompassword =
+    randompassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_four.length));
+  randompassword =
+    randompassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+  randompassword =
+    randompassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_two.length));
+  randompassword =
+    randompassword +
+    collection_three.charAt(
+      Math.floor(Math.random() * collection_three.length)
+    );
+  randompassword =
+    randompassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_four.length));
+  randompassword =
+    randompassword +
+    collection_one.charAt(Math.floor(Math.random() * collection_one.length));
+  randompassword =
+    randompassword +
+    collection_two.charAt(Math.floor(Math.random() * collection_two.length));
+  randompassword =
+    randompassword +
+    collection_three.charAt(
+      Math.floor(Math.random() * collection_three.length)
+    );
+  randompassword =
+    randompassword +
+    collection_four.charAt(Math.floor(Math.random() * collection_four.length));
   display.value = randompassword;
 }
